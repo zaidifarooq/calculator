172f47b2b41a163665682752e0772c8eefbfd8e1
zaidifarooq <154751421+zaidifarooq@users.noreply.github.com>
2023-12-24 22:22:40 +0500

    adding files

---
+++ calcu.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        type: 'number',
+        name: 'num1',
+        message: 'enter your first number'
+    },
+    {
+        type: 'number',
+        name: 'num2',
+        massage: 'enter your last number'
+    },
+    {
+        type: "list",
+        name: "operator",
+        massage: "enter the operator",
+        choices: ["add", "subtract", "multiply", "divide"]
+    }
+]);
+let result;
+switch (answer.operator) {
+    case "add":
+        result = answer.num1 + answer.num2;
+        console.log(result);
+        break;
+    case "subtract":
+        result = answer.num1 - answer.num2;
+        console.log(result);
+        break;
+    case "multiply":
+        result = answer.num1 * answer.num2;
+        console.log(result);
+        break;
+    case "divide":
+        result = answer.num1 / answer.num2;
+        console.log(result);
+        break;
+}
